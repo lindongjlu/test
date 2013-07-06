b9e2cea1881787e19561cd5c9e22097b6c9a2272
lindongjlu <lindongjlu@gmail.com>
2013-07-06 10:22:05 +0800

    1

---
+++ cpp/TestCLib/TestCStdio.cpp
@@ -0,0 +1,16 @@
+/*
+ * TestCStdio.cpp
+ *
+ *  Created on: 2013Äê7ÔÂ6ÈÕ
+ *      Author: francislin
+ */
+
+#include <cstdio>
+
+int main(int argn, char** argv)
+{
+	printf("Hello world!");
+	return 0;
+}
+
+

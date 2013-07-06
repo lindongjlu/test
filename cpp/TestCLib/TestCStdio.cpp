3f179f053a6d843cf5c35e9188f309df85e8d224
lindongjlu <lindongjlu@gmail.com>
2013-07-06 10:54:05 +0800

    1

--- cpp/TestCLib/TestCStdio.cpp
+++ cpp/TestCLib/TestCStdio.cpp
@@ -1,8 +1,37 @@
 
 #include <cstdio>
 
+void operationOnFiles();
+
 int main(int argn, char** argv)
 {
-	printf("Hello world!");
+	operationOnFiles();
 	return 0;
+}
+
+void operationOnFiles() {
+
+	if (remove("abc.txt") != 0) {
+		perror("remove error");
+	} else {
+		puts("remove succ");
+	}
+
+	if (rename("abc.txt", "cba.txt") != 0) {
+		perror("rename error");
+	} else {
+		puts("rename succ");
+	}
+
+	FILE * pFile = tmpfile();
+	if (pFile != NULL) {
+		puts("create tmp file succ");
+		fclose(pFile);
+	}
+
+	char buffer[L_tmpnam];
+	if (tmpnam(buffer) != NULL) {
+		printf("create tmp file %s succ", buffer);
+	}
+
 }

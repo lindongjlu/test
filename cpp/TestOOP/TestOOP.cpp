71d01479a79ec2b6240ef358269ed42ceb641eb7
lindongjlu <lindongjlu@gmail.com>
2013-08-07 09:47:38 +0800

    1

---
+++ cpp/TestOOP/TestOOP.cpp
@@ -0,0 +1,19 @@
+/*
+ * TestOOP.cpp
+ *
+ *  Created on: 2013Äê8ÔÂ7ÈÕ
+ *      Author: francislin
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argn, char** argv) {
+
+	cout<< "Hello world!"<<endl;
+}
+
+
+
+
